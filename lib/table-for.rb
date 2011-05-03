30a271cfcba0d6cee2ca7bf5cb444748c77607ac
hunterae <hunterae@gmail.com>
2011-05-03 15:48:06 -0400

    Getting railties engine working

--- lib/table-for.rb
+++ lib/table-for.rb
@@ -1,2 +1,13 @@
+require "action_view"
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require "table_for/table_for"
-require "table_for/engine"
+require "table_for/helper_methods"
+# require "table_for/engine"
+
+$LOAD_PATH.shift
+
+if defined?(ActionView::Base)
+  ActionView::Base.send :include, TableFor::HelperMethods
+end
--- lib/table_for/engine.rb
+++ lib/table_for/engine.rb
@@ -3,10 +3,10 @@ require "table-for"
 
 module TableFor
   class Engine < Rails::Engine
-    initializer "table_for.initialize_helpers" do
-      ActiveSupport.on_load(:action_view) do
-        include TableFor::HelperMethods
-      end
-    end
+    # initializer "table_for.initialize_helpers" do
+      # ActiveSupport.on_load(:action_view) do
+      #         include TableFor::HelperMethods
+      #       end
+    # end
   end
 end
